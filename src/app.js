56e6bb6b364fd5bc824dc36f894178c3eb4e7b6e
Henry Leon <helg18@gmail.com>
2018-09-16 20:02:11 -0400

    Removiendo llamadas a API desde el controlador principal

--- src/app.js
+++ src/app.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-
- // Services
-import API from './services/api';
 
 // Home Layouts
 import Home from '../home/containers/home-layout';
@@ -12,17 +8,11 @@ import Characters from '../characters/containers/characters-layout';
 
 
 class MainLayout extends Component {
-  async componentDidMount(){
-    const characters = await API.getCharacters().catch((error)=>{
-      console.log("Api call error");
-      alert(error.message);
-    });
-  };
 
   render(){
     return(
-      // <Characters/>
-      <Home/>
+      <Characters/>
+      // <Home/>
       );
   };
 }
